77f263b1467c9bdd77ec41fbb0c38c17e1f8a311
r1gel0629 <h2530120@gl.cc.uec.ac.jp>
2025-04-10 21:07:06 +0900

    初期状態に変更

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0); // 状態（state）を定義
-
-  const handleClick = () => {
-    setCount(count + 1); // ボタンがクリックされたら count を1増やす
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>現在のカウント: {count}</p>
-        <button onClick={handleClick}>
-          カウントを増やす
-        </button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
